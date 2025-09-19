81ba9d60468c030c32c6e71106ec5ebdabefb916
/ai.oli <aioli.collective@gmail.com>
2025-09-19 23:52:53 +0200

    Mise à jour imagevisualizer v6

--- visualizerimages.js
+++ visualizerimages.js
@@ -1,5 +1,6 @@
 const images = [
 https://res.cloudinary.com/aioli/image/upload/v1758318340/aioli.collective/ymy4xsgui0fdetj5ez12.webp
 https://res.cloudinary.com/aioli/image/upload/v1758318574/aioli.collective/sdx45xspmoduna6nxzh8.png
+https://res.cloudinary.com/aioli/image/upload/v1758318772/aioli.collective/fnrjj1dubi6ic4cagaje.jpg
 //ANCHOR
 ];
