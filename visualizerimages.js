a7656e8289f72f8ecf5a1e7332655cb27829ff75
/ai.oli <aioli.collective@gmail.com>
2025-09-19 23:49:35 +0200

    Mise à jour index.html v4

--- visualizerimages.js
+++ visualizerimages.js
@@ -1,4 +1,5 @@
 const images = [
 https://res.cloudinary.com/aioli/image/upload/v1758318340/aioli.collective/ymy4xsgui0fdetj5ez12.webp
+https://res.cloudinary.com/aioli/image/upload/v1758318574/aioli.collective/sdx45xspmoduna6nxzh8.png
 //ANCHOR
 ];
